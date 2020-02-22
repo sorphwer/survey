2b82a1871565aae1a1060f03277a6d108c2d02bf
Riino <sorphwer@gmail.com>
2020-02-22 16:09:33 +0800

    update 2.22

--- app/src/main/java/mg/studio/android/survey/MainActivity.java
+++ app/src/main/java/mg/studio/android/survey/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.weclome);
+        setContentView(R.layout.question_one);
+
     }
+
 }
